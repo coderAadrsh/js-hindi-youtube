fb662cec8bbd92412ebf5ea9b8b0670c2e1213d7
ADARSH KUMAR SINGH <158481652+coderAadrsh@users.noreply.github.com>
2024-11-22 13:49:44 +0000

    datatypes of js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId =144553
+let accountEmail ="abcd@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+let accountState;//undefined
+accountEmail ="qwe@gmail.com"
+accountPassword="76543"
+accountCity="bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using nodejs ,not browser 
+
+// number=> 2 to power 53
+//bigint
+//string
+//boolean=> trur,false
+//null=.standalone value
+//undefine=>
+    //symbol=> unique
+
+
+//object
+ let age=22
+console.log(typeof null)
