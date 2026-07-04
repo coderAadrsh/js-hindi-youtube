b6eb4c0966b26233959fec6d7d76fbcb0dd1a0a4
ADARSH KUMAR SINGH <158481652+coderAadrsh@users.noreply.github.com>
2026-07-04 06:31:28 +0000

    logical conversion

--- 01_basics/03_comparision.js
+++ 01_basics/03_comparision.js
@@ -1,14 +1,35 @@
+let score =null
+
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)// NaN not a number, score=null=>0,score=undefine=>NaN
+
+// "33" =>33
+//"33abc" =>NaN
+//true =>1
+//false =>0
+
 // console.log("2">1);
 // console.log(2>"1");
 
-// console.log(null>0);
-// console.log(null==0);
-// console.log(null<0);
-// console.log(null>=0);
-// console.log(null<=0);
+console.log("1"+2)//12
+console.log(1+"2")//12
 
-console.log(undefined<0);
-console.log(undefined>0);
-console.log(undefined==0);
-console.log(undefined<=0);
-console.log(undefined>=0);
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log(+true)//1  true+=error 
+console.log(+"")//0
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
+
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
