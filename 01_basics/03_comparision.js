e98b28f2c323922febc10db9732fe7e41811f1f3
ADARSH KUMAR SINGH <158481652+coderAadrsh@users.noreply.github.com>
2026-07-07 14:36:33 +0000

    comparision

--- 01_basics/03_comparision.js
+++ 01_basics/03_comparision.js
@@ -11,22 +11,27 @@ console.log(valueInNumber)// NaN not a number, score=null=>0,score=undefine=>NaN
 //true =>1
 //false =>0
 
+
+// avoid these comparision 
+
 // console.log("2">1);
 // console.log(2>"1");
 
-console.log("1"+2)//12
-console.log(1+"2")//12
+// console.log("1"+2)//12
+// console.log(1+"2")//12
 
-console.log("1"+2+2)//122
-console.log(1+2+"2")//32
-console.log(+true)//1  true+=error 
-console.log(+"")//0
+// console.log("1"+2+2)//122
+// console.log(1+2+"2")//32
+// console.log(+true)//1  true+=error 
+// console.log(+"")//0
 
 console.log(null>0);
 console.log(null==0);
 console.log(null<0);
 console.log(null>=0);
 console.log(null<=0);
+
+console.log("-----------________________-----------")
 
 // console.log(undefined<0);
 // console.log(undefined>0);
